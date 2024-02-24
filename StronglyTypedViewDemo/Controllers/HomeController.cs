465b64f738c5aa6a23851601b68e72a199283370
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-02-24 22:05:06 +0530

    Add project files.

---
+++ StronglyTypedViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using StronglyTypedViewDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypedViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee emp1 = new Employee();
+            emp1.Id = 11;
+            emp1.Name = "Ali Khan";
+            emp1.Age = 25;
+
+            ViewData["Var1"] = emp1;
+            ViewBag.Var2 = emp1;
+
+            return View();
+        }
+    }
+}
---
+++ StronglyTypedViewDemo/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedViewDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
